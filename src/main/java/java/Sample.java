713435df1661330d1ceeb85ec6eb3330cfd617c9
Luca Scagliola <luca.scagliola@gmx.de>
2023-10-12 19:54:36 +0200

    Hopefully working version

--- src/main/java/java/Sample.java
+++ src/main/java/java/Sample.java
@@ -37,9 +37,9 @@ public class Sample {
 
         JsonObject object = element.getAsJsonObject();
 
-        //if (object.get("name") == null) {
-        //    throw new IllegalArgumentException("input must have key \"name\"");
-        //}
+        if (object.get("name") == null) {
+            throw new IllegalArgumentException("input must have key \"name\"");
+        }
 
         return object.get("name").getAsString();
     }
